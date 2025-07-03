7283a2d8f9a8b90879ae58b42023c6abc27262ee
Davi Monteiro <montsgit@gmail.com>
2025-07-03 16:12:33 -0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:crypto/common/constants/app_theme.dart';
+import 'package:crypto/features/home/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crypto',
+      theme: AppTheme.lightTheme,
+      home: const HomePage(title: 'Bitcoin'),
+    );
+  }
+}
---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color bitcoinColor = Color(0xFFF7931A);
+}
