9d2b227f1627c9ff7219fe2fb7f7c89b023333fc
Davi Monteiro <montsgit@gmail.com>
2025-07-09 11:54:48 -0300

    feat: show coins

---
+++ lib/features/home/data/models/coin_model.dart
@@ -0,0 +1,12 @@
+class Coin {
+  String icon;
+  String name;
+  String id;
+  double price;
+  Coin({
+    required this.icon,
+    required this.name,
+    required this.id,
+    required this.price,
+  });
+}
---
+++ lib/features/home/data/repositories/coin_repository.dart
@@ -0,0 +1,37 @@
+import 'package:crypto/features/home/data/models/coin_model.dart';
+
+class CoinRepository {
+  static List<Coin> coinsArray = [
+    Coin(
+      icon: 'assets/icons/bitcoin.png',
+      name: 'Bitcoin',
+      id: 'BTC',
+      price: 600000.0,
+    ),
+    Coin(
+      icon: 'assets/icons/cardano.png',
+      name: 'Cardano',
+      id: 'ADA',
+      price: 3.0,
+    ),
+    Coin(
+      icon: 'assets/icons/ethereum.png',
+      name: 'Ethereum',
+      id: 'ETH',
+      price: 14000.0,
+    ),
+    Coin(
+      icon: 'assets/icons/litecoin.png',
+      name: 'Litecoin',
+      id: 'LTC',
+      price: 500.0,
+    ),
+    Coin(
+      icon: 'assets/icons/usdcoin.png',
+      name: 'USD Coin',
+      id: 'USDC',
+      price: 6.0,
+    ),
+    Coin(icon: 'assets/icons/xrp.png', name: 'XRP', id: 'XRP', price: 12.0),
+  ];
+}
